2af187ef79e0598a416bcfa2a0010270b5cf1c09
soifant <soifantoko222@gmail.com>
2023-02-05 21:36:20 +0700

    perbaikan

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'reach';
+import ReachDOM from 'react-dom/client';
+
+const myFirstElement = <h1>Hallo word</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
