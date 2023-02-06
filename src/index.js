401598f5f82c49b820809e33f889f4776c3378b2
soifant <soifantoko222@gmail.com>
2023-02-06 14:34:56 +0700

    pembaruan

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ function Lang(props){
 	return <h2> This a {props.lang} language</h2>;
 }
 
+function Code(){
+	return {
+	<>
+		<h1> What your favorite code</h1>
+		<Lang />
+	</>
+	}
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Lang lang="nodeJs"/>);
+root.render(<Code />);
