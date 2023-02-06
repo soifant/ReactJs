398853a871c5b8505b1a27d75caeec8522b0f9da
soifant <soifantoko222@gmail.com>
2023-02-06 16:54:04 +0700

    pemabaruan

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Lang(props){
-	return <h3> Languange {props.lang}</h3>;
+	return <h3> Languange {props.lang} and framework {props.fm}</h3>;
 }
 
 function Code(){
-	const lang = ['javascript', 'php', 'nodrjs'];
+	const lang = [
+	
+		{lang : 'javascript', fm : 'ReactJs'}, 
+		{lang : 'php', fm : 'Laravel'}, 
+		{lang : 'python', fm : 'Django'}
+		
+	];
 	
 	return (
 		<>
-			<h1> You can computer languange</h1>
+			<h1> You write code with</h1>
 			<ul>
-				{lang.map((lang) => <Lang lang={lang}/>)}
+				{lang.map((lang) => <Lang lang={lang.lang} Fm={lang.fm}/>)}
 			</ul>
 		</>
 	)
