09b268db514d3c6237d133f43e64376898159434
soifant <soifantoko222@gmail.com>
2023-02-06 10:24:47 +0700

    pembaruan

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const x = 5;
-const element = (
-<>
-	<h2> Selamat datang </h2>
-	<br/>
-	<h1>Ini perkalian {x} x {x} = {x*x}</h1>
-	<br/>
-	<small> 
-	Hasil perkalian 
-	{(x) < 5 ? "lebih kecil" : "lebih besar"}
-	{x}
-	</small>
-</>
-
-);
+function Lang(){
+	return <h2> This a javascript language</h2>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<Lang />);
