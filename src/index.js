d971505526c530f04578dd3674251ac479a3e99e
soifant <soifantoko222@gmail.com>
2023-02-06 09:57:47 +0700

    pembaruan

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client';
 const element = (
 	<div>
 		<h1>Ini adalah angka 1</h1>
-		<br>
-		<h1>ini adalah perkalian {10 * 20}</h1>
-		<br>
+		<br/>
+		<h1>ini adalah perkalian {10 * 20} </h1>
+		<br/>
 		<small> Anda sudah menlihat keduanya</small>
 	</div>
 );
