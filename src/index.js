a80754a631e6c4769ca890a5c485d0f46f6dc78d
soifant <soifantoko222@gmail.com>
2023-02-06 09:53:04 +0700

    pembaruan

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = <h1> ini ada angka perkalian {5 + 5}</h1>;
+const element = (
+	<div>
+		<h1>Ini adalah angka 1</h1>
+		<br>
+		<h1>ini adalah perkalian {10 * 20}</h1>
+		<br>
+		<small> Anda sudah menlihat keduanya</small>
+	</div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
