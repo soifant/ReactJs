0f1bc66b64963a34dac9324bc8105ba48403c518
soifant <soifantoko222@gmail.com>
2023-02-06 09:31:06 +0700

    pembaruan

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-<<<<<<< HEAD
-const myelement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-);
-
-ReactDOM.render(myelement, document.getElementById('root'));
-=======
 const element = <h1> ini tampilan home</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
->>>>>>> origin/master
