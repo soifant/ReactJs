98755786028cca3637420b59ece230523e6801f1
soifant <soifantoko222@gmail.com>
2023-02-06 14:48:37 +0700

    pembaruan

---
+++ src/Code.js
@@ -0,0 +1,5 @@
+function Lang(props){
+	return <h1> Your code read with {props.lang} languange</h1>
+}
+
+export default Lang;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function Lang(props){
-	return <h2> This a {props.lang} language</h2>;
-}
-
-function Code(){
-	return (
-	<>
-		<h1> What your favorite code</h1>
-		<Lang lang="JavaScript"/>
-	</>
-	)
-}
+import Code from './Code.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Code />);
+root.render(<Code lang="javascript"/>);
