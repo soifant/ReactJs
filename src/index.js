fb3adf09b27e8df0ad07488abdc54550ff83d4dc
soifant <soifantoko222@gmail.com>
2023-02-06 18:54:40 +0700

    pembaruan

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Form(){
 
+	conat [name, setName] = useState("");
+	
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		alert(`Your name is ${name}`);
+	}
+	
 	return(
 		<form>
 			<label> Enter your name here
-				<input type='text'/>
+				<input type='text'
+				value = {name}
+				onChange ={ (e) => setName(e.target.value)}
+				/>
 			</label>
+			<input type="submit"/>
 		</form>
 	);
 }
