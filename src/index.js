8375240eecf3a03ac5d33f2e1b4f581faf6516cb
soifant <soifantoko222@gmail.com>
2023-02-06 09:36:53 +0700

    pembaruan

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = <h1> ini tampilan home</h1>;
+const element = <h1> ini ada angka perkalian {5 + 5}</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
